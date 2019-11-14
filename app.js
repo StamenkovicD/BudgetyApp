3f55af93476d68aff3a63f0608bf8d32f6d371a6
StamenkovicD <40769722+StamenkovicD@users.noreply.github.com>
2019-11-14 18:11:19 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+//BUDGER CONTROLER
+var budgetController = (function() {
+   
+
+}) ();
+
+
+//UI CONTROLLER
+var UIController = (function(){
+
+
+}) ();
+
+
+//GLOBAL CONTROLER
+var controller = (function(budgetyCtrl, UICtrl){
+
+    var ctrlAddItem = function() {
+
+        //1. Get the field input data
+
+        //2. Add the item  to the budget controller 
+
+        //3. Add the item to the UI
+
+        //4. Calculate the budget
+
+        //5. Display the budget on the UI
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        if(event.charCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        };
+    })
+    
+})(budgetController, UIController);
